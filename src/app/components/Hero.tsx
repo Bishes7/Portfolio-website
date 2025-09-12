892779a9d1106857789c1502999a1c45064225b0
Bishes Adhikari <specialbishes9@gmail.com>
2025-09-12 23:20:42 +1000

    hero section updated

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="py-28 container max-w-7xl mx-auto px-4">
+      <div className="max-w-3xl mx-auto text-center">
+        <div className="flex flex-col items-center mb-4">
+          <Image
+            src="/profile.avif"
+            alt="profile"
+            height={100}
+            width={100}
+            className="rounded-full mb-4 w-32 h-32 object-cover ring-2 ring-primary"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
