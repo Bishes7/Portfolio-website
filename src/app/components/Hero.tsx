97214d8d3f23446b0df31501b455895dc4e16791
Bishes Adhikari <specialbishes9@gmail.com>
2025-09-13 20:59:13 +1000

    project section created

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -13,6 +15,50 @@ const Hero = () => {
             width={100}
             className="rounded-full mb-4 w-32 h-32 object-cover ring-2 ring-primary"
           />
+        </div>
+        <h1 className="text-4xl md:text-6xl font-bold mb-6 ">
+          Hi, I'm <span className="text-primary">Bishes Adhikari</span>{" "}
+        </h1>
+        <p className="text-xl md:text-2xl text-gray-600 dark:text-gray-300 mb-8  font-bold">
+          Full Stack Developer | UI/UX Enthusiast | Open Source Contributor
+        </p>
+        <div className="flex justify-center space-x-4 mb-8">
+          <Link
+            href=""
+            className="text-2xl text-gray-600
+           hover:text-primary dark:text-gray-300 transition-colors duration-300  "
+          >
+            <FaGithub />
+          </Link>
+          <Link
+            href=""
+            className="text-2xl text-gray-600
+           hover:text-primary dark:text-gray-300 transition-colors duration-300  "
+          >
+            <FaLinkedin />
+          </Link>
+          <Link
+            href=""
+            className="text-2xl text-gray-600
+           hover:text-primary dark:text-gray-300 transition-colors duration-300  "
+          >
+            <FaFacebook />
+          </Link>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center gap-4 ">
+          {" "}
+          <Link
+            href=""
+            className="bg-primary inline-block w-full md:w-auto text-white px-8 py-3 rounded-lg hover:bg-primary/80 transition-colors"
+          >
+            View Projects
+          </Link>
+          <Link
+            href=""
+            className="bg-yellow-400 inline-block w-full md:w-auto text-gray-800 px-8 py-3 rounded-lg hover:bg-amber-300 transition-colors"
+          >
+            Contact Me
+          </Link>
         </div>
       </div>
     </section>
