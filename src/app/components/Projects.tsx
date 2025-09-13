02c7c83fc7e048a595cd0cad9544e0db66021dc8
Bishes Adhikari <specialbishes9@gmail.com>
2025-09-13 22:36:55 +1000

    project section updated

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,9 +1,28 @@
+import { projects } from "@/contents/projects";
+import Image from "next/image";
 import React from "react";
 
 const Projects = () => {
   return (
     <div className="py-20 container ">
-      <h2>Featured Projects</h2>
+      <h2 className="text-3xl font-bold mb-12 text-center ">
+        Featured Projects
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
+        {projects.map((project) => (
+          <article key={project.title}>
+            <div className="relative aspect-video mb-4 rounded-lg overflow-hidden ">
+              <Image
+                src={project.image}
+                alt={project.title}
+                fill
+                className="object-cover"
+                sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw"
+              />
+            </div>
+          </article>
+        ))}
+      </div>
     </div>
   );
 };
--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -11,7 +11,7 @@ export const projects: Project[] = [
     image: "/projects/e-commerce-website.png",
   },
   {
-    title: "Task Management App",
+    title: "RealEstate App",
     description:
       "A collaborative task management application with real-time updates.",
     technologies: ["React", "Node.js", "MongoDB", "Socket.io"],
@@ -38,34 +38,19 @@ export const projects: Project[] = [
     image: "/projects/blog-website.jpeg",
   },
   {
-    title: "Weather App",
+    title: "Library Management System App",
     description:
       "A weather application that provides real-time weather updates.",
-    technologies: ["React", "OpenWeatherMap API"],
+    technologies: ["React", "Node", "MongoDb", "Express"],
     githubLink: "https://github.com",
     demoLink: "https://demo.com",
     image: "/projects/weather-app.png",
   },
-  {
-    title: "Chat Application",
-    description: "A real-time chat application using WebSocket technology.",
-    technologies: ["React", "Node.js", "Socket.io"],
-    githubLink: "https://github.com",
-    demoLink: "https://demo.com",
-    image: "/projects/chat-app.png",
-  },
-  {
-    title: "Recipe Finder",
-    description: "A recipe search application using the Edamam API.",
-    technologies: ["React", "Edamam API"],
-    githubLink: "https://github.com",
-    demoLink: "https://demo.com",
-    image: "/projects/recipe-finder.png",
-  },
+
   {
     title: "Expense Tracker",
     description: "A personal finance tracker to manage expenses and income.",
-    technologies: ["React", "Firebase"],
+    technologies: ["React", "Node", "MongoDb", "Express"],
     githubLink: "https://github.com",
     demoLink: "https://demo.com",
     image: "/projects/expense-tracker.webp",
