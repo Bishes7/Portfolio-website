b6c6cc664d6dc0352b5eb5ba0c9c248d0943de10
Bishes Adhikari <specialbishes9@gmail.com>
2025-09-14 20:39:46 +1000

    home page completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import { ThemeProvider } from "./context/ThemeContext";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,7 @@ export default function RootLayout({
           <NavBar />
 
           <main className="min-h-screen pt-24">{children}</main>
-          <footer>Footer</footer>
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
